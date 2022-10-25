1503ae154710d025ffb63ca061b5db2a9689dff7
Mezan2002 <mezanurr009@gmail.com>
2022-10-25 20:45:04 +0600

    Selected Items card design almost dome

--- src/components/SelectedCourse/SelectedCourse.js
+++ src/components/SelectedCourse/SelectedCourse.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import { FaClock, FaStar, FaUser, FaVideo } from "react-icons/fa";
 
 const SelectedCourse = () => {
   const selectedCourse = useLoaderData();
@@ -22,9 +23,40 @@ const SelectedCourse = () => {
           <div className="px-20 text-left">
             <p className="text-4xl font-bold">{courseName}</p>
             <p className="mt-5">{courseDescription}</p>
-            <div className="mt-5 flex">
-              <p>{rating}</p>
-              <p>Capacity of students: {student}</p>
+            <div className="mt-5 flex items-center justify-between">
+              <div className="flex">
+                <p className="mt-5">
+                  <FaStar></FaStar>
+                </p>
+                <p className="mt-5">
+                  <FaStar></FaStar>
+                </p>
+                <p className="mt-5">
+                  <FaStar></FaStar>
+                </p>
+                <p className="mt-5">
+                  <FaStar></FaStar>
+                </p>
+                <p className="mt-5">
+                  <FaStar></FaStar>
+                </p>
+              </div>
+              <div>
+                <div className="mt-5 flex items-center">
+                  <span className="mr-2">
+                    <FaUser></FaUser>
+                  </span>
+                  <p>{student}</p>
+                </div>
+              </div>
+              <div>
+                <div className="flex items-center">
+                  <span className="mr-2">
+                    <FaVideo></FaVideo>
+                  </span>
+                  <p>{lessons}</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
